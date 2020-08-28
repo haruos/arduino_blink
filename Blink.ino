3f2a3748cce6d28b3dddee3aaf7916bacca7c603
Haruo Shibuya <haruos@microsoft.com>
2020-08-28 16:56:16 +0900

    First Commit

--- Blink.ino
+++ Blink.ino
@@ -25,6 +25,7 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
+  // Modified 1
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
